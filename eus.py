14a01c57c5efb68a02b07db2d15783a872d187b6
Kim-Wangyu <wang90205@gmail.com>
2022-07-14 11:20:51 +0900

    차트 마우스hover정보

---
+++ eus.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from pandas_datareader import data as pdr
+import yfinance as yf
+import datetime as dt
+import plotly.graph_objs as go
+
+# 재정의
+yf.pdr_override()
+
+# 주식코드 입력받기
+stock=input("Enter a stock ticker symbol: ")
+
+# 1m 간격으로 yfinance API에서 재고 데이터 프레임(df) 검색
+df = yf.download(tickers=stock,period='1d',interval='1m')
+
+print(df)
+
+# 그래프
+fig=go.Figure()
+
+fig.add_trace(go.Candlestick(x=df.index,
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'], name = 'market data'))
+
+fig.update_layout(
+    title= str(stock)+' Live Share Price:',
+    yaxis_title='Stock Price (USD per Shares)')              
+
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=15, label="15m", step="minute", stepmode="backward"),
+            dict(count=45, label="45m", step="minute", stepmode="backward"),
+            dict(count=1, label="HTD", step="hour", stepmode="todate"),
+            dict(count=3, label="3h", step="hour", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+fig.show()
