5ab40cffd0a322b5fffaa1bfe84821756baec8ec
yuseon <dksshd44@gmail.com>
2022-07-19 13:44:14 +0900

    2페이지 커밋, 3페이지 연결 경로 수정

--- main.py
+++ main.py
@@ -13,14 +13,14 @@
 
 # @app.get("/trading")
 # async def one():
-    
+
 
 #     return {"주식 Top 5":one_page_list(1,1)}
 
 
 from fastapi.staticfiles import StaticFiles
 from typing import Union, ValuesView
-from  fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 from fastapi import FastAPI, Request
@@ -32,21 +32,23 @@ app = FastAPI()
 
 app.mount(
     "/static",
-    StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"),
+    StaticFiles(directory=Path(__file__).parent.absolute() / "static"),
     name="static",
 )
 
 templates = Jinja2Templates(directory="templates")
 
-
 @app.get("/")
 async def root(request: Request):
-    return templates.TemplateResponse(
-        "trading.html", {"request": request}
-    )
+    return "main page"
 
+@app.get("/trading")
+async def root(request: Request):
+    return templates.TemplateResponse("trading.html", {"request": request})
 
-
+@app.get("/index")
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 # @app.get("/")
@@ -55,7 +57,7 @@ async def root(request: Request):
 
 # @app.get("/trading")
 # async def one():
-    
+
 
 #     return {"주식 Top 5":one_page_list(1,1)}
 
@@ -69,7 +71,7 @@ async def root(request: Request):
 #     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 #     <link rel="stylesheet" href="./css/reset.css">
 #     <link rel="stylesheet" href="./css/style.css">
-#     <link rel="stylesheet" href="./css/trading.css"> 
+#     <link rel="stylesheet" href="./css/trading.css">
 #     <title>트리플 이응 - 주식정보</title>
 #     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 #     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.8.0/chart.min.js"></script>
@@ -125,4 +127,3 @@ async def root(request: Request):
 # </body>
 # </html>"""
 #     return HTMLResponse(content=html_content,status_code=200)
-
