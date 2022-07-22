6c3d9b587d18f054e15b0b1fdb37d8915e22d78f
Kim-Wangyu <wang90205@gmail.com>
2022-07-22 13:49:52 +0900

    index페이지 링크, 상세페이지 종목명,코드,현재가 데이터연결

--- fdr_test.py
+++ fdr_test.py
@@ -9,25 +9,19 @@ def prac():
     global d
     d = date(2022, 1, 11)
     e = date(2022, 7, 8)
-    print(d.isoformat()+"www")
     global cal
 
     dat =[]
     df = fdr.DataReader('005930', d.isoformat())
     
     dat=df['Close'].tolist()
-    #print(dat)
     indexList= df.index.strftime("%Y-%m-%d").tolist()
   
     STOCK_LIST=[]
     
-    print(len(df.index))
-    print(len(dat))
-    
     for i in range(len(dat)):       
         stockInfo = [str(indexList[i]),dat[i]]
         STOCK_LIST.append(stockInfo)
-        print(stockInfo)
         df = pd.DataFrame(STOCK_LIST, columns=('기간','종가'))
         js = df.to_json(orient='columns').encode().decode('unicode-escape')
     return js
--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-from ast import Import
-from datetime import date
-from turtle import st
-
 from fastapi.staticfiles import StaticFiles
-from typing import List, Optional, Union, ValuesView
-from  fastapi.responses import HTMLResponse
+from typing import Optional
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
-from fastapi import FastAPI, Query, Request
-from h11 import Data
-from pydantic import BaseModel
-from pyparsing import html_comment
+from fastapi import FastAPI, Request
 from fdr_test import prac
-from topNaver import STOCK_NAME_LIST, one_page_list
+from topNaver import one_page_list
 from fdr_testCopy import prac
 import sys
 sys.path.append("/fdr_testCopy")
 import fdr_testCopy as ii
 sys.path.append("/topNaver")
 import topNaver as tn
-from bs4 import BeautifulSoup as bs4
+import json
 
 
 listSt=[]
@@ -39,27 +31,53 @@ app.mount(
 
 
 templates = Jinja2Templates(directory="templates")
-
-
 @app.get("/")
 async def root(request: Request):
     return "main page"
 
-
-
 @app.get("/index")
-async def root(request: Request,q:Optional[list]=listSt):  
+async def root(request: Request): 
+
+    top_5_list=one_page_list(1,1)
+    red=json.loads(top_5_list)
+
+    topNameList=[]
+    topCodeList=[]
+    for k_name, k_code in zip(red['종목명'], red['code']):
+        
+        topNameList.append(red['종목명'][k_name])
+        topCodeList.append(red['code'][k_code])
+
+    payload={
+        "name": topNameList,
+        "code": topCodeList,
+    }
+
     return templates.TemplateResponse(
-        "index.html", {"request": request, "q":q})
-    
+        "index.html", {"request": request, "payload":payload})
 
 @app.get("/trading")
-async def root(request: Request,q:Optional[str]=ii.currentStock):
-    print(prac()+"dddddd")
-    return templates.TemplateResponse(
-        "trading.html", {"request": request,"q":q, "ooo": prac()}
-    )
+async def root(
+    request: Request, code:Optional[str]=None
+):
+    top_5_list = one_page_list(1,1)
+    res= json.loads(top_5_list)
+    bb=[]
+    for k_name, k_price, k_code in zip(res['종목명'], res['현재가'], res['code']):
+        if(code==res['code'][k_code]):
+            bb.append(res['종목명'][k_name])
+            bb.append(res['현재가'][k_price])
+            bb.append(res['code'][k_code])
+            break
+    payload = {
+        "name": bb[0],
+        "close_price": bb[1],
+        "price_list": prac(bb[2]),
+    }
 
+    return templates.TemplateResponse(
+        "trading.html", {"request": request, "payload": payload }
+    )
 
 @app.get("/prac")
 async def root(request: Request,q:Optional[str]=ii.currentStock):
