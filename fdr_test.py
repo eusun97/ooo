d88582d2ade7c3be7dcb0f4d8fa23c54c0862a14
yuseon <dksshd44@gmail.com>
2022-07-22 17:07:36 +0900

    최종

--- fdr_test.py
+++ fdr_test.py
@@ -1,33 +1,30 @@
-
 from datetime import date, datetime
 from operator import index
 import FinanceDataReader as fdr
 import datetime
 import pandas as pd
 
-# 삼성전자(005930)
-def prac():
+# 삼성전자(005930) 전체 (1996-11-05 ~ 현재)
+# df = fdr.DataReader('005930')
+
+def prac(code: str="005930"):
     global d
     d = date(2022, 1, 11)
+    global currentStock  
     dat =[]
-    df = fdr.DataReader('005930', d.isoformat())
-
-    
-    
+    df = fdr.DataReader(code, d.isoformat())
     dat=df['Close'].tolist()
-
-
+    currentStock=dat[-1]
+   
     indexList= df.index.strftime("%Y-%m-%d").tolist()
 
-   
     STOCK_LIST=[]
-    
-    for i in range(len(dat)):       
+    for i in range(len(dat)):
+        
         stockInfo = [str(indexList[i]),dat[i]]
         STOCK_LIST.append(stockInfo)
         df = pd.DataFrame(STOCK_LIST, columns=('기간','종가'))
         js = df.to_json(orient='columns').encode().decode('unicode-escape')
-
     return js
 
 prac()
